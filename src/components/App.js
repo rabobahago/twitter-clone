3157a089cc0e18057040b7ba93815faf97f694b8
rabobahago <rabobahago@gmail.com>
2021-09-25 16:19:49 +0100

    loading bar was added in order to show loading component when data is not ready on fetch

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,19 @@ import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { handleInitialData } from "../actions/shared";
 import Dashboard from "./Dashboard";
+import LoadingBar from "react-redux-loading";
 
 const App = ({ dispatch, loading }) => {
   useEffect(() => {
     dispatch(handleInitialData());
   }, [dispatch]);
 
-  return <div>{loading === true ? null : <Dashboard />}</div>;
+  return (
+    <div>
+      <LoadingBar />
+      {loading === true ? null : <Dashboard />}
+    </div>
+  );
 };
 
 const mapStateToProps = ({ authedUser }) => {
